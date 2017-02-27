18c8bc3cbecc151909e72b2355b59ac2043f853e
Patrick Ryan <pat@timehero.com>
2017-02-26 19:52:48 -0500

    RASPBERRY PI
    Making button use pulldown resistor instead of pulp

--- raspberrypi/examples/02_button_example.py
+++ raspberrypi/examples/02_button_example.py
@@ -11,7 +11,7 @@ Required components:
 Instructions:
     1. Connect the Pi Cobbler to the Raspberry Pi and the breadboard
     2. Connect a wire from pin #18 to one side of the button
-    3. Connect a wire from the other side of the button to GND
+    3. Connect a wire from the other side of the button to 3V3
     4. Click Run -> Run Module or press F5
 
 """
@@ -20,13 +20,14 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 # The pin we will be connecting to our button
 button = 18
 
 # Tell the Pi that we are using the pin as an input
 # to read the status of a push button
-GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(button, GPIO.IN, GPIO.PUD_DOWN)
 
 print 'Running program...'
 
@@ -37,7 +38,7 @@ while True:
     # Read the status (voltage) of the pin
     buttonState = GPIO.input(button)
 
-    if buttonState == GPIO.LOW:
+    if buttonState == GPIO.HIGH:
         print ('Button pressed')
     else:
         print ('Button released')
